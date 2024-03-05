dd4983090c35b2845043de0a8d84425cee3d610a
jp1405 <izairi.q@gmail.com>
2024-03-05 01:00:04 +0100

    implementing invoicing - generating invoices. ProductDetailsDTO not working as an array. updated sql dump

--- src/main/java/com/crm/wm/dto/InvoiceRequestWithoutDefaultDTO.java
+++ src/main/java/com/crm/wm/dto/InvoiceRequestWithoutDefaultDTO.java
@@ -8,6 +8,6 @@ public class InvoiceRequestWithoutDefaultDTO {
     private Long customerId;
     private Long employeeId;
     private Long municipalityId;
-    private List<InvoiceItemRequestDTO> invoiceItemRequestDTOs;
+    private List<Long> productIds;
     private Boolean isPaid;
 }
